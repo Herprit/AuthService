9219b313f9c148e693783ac529c428342d29f886
herprit <herpritsingh12@gmail.com>
2021-03-30 21:42:21 +0200

    Add project files.

---
+++ AuthService/Controllers/TokenController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly ILogger<TokenController> _logger;
+
+        public TokenController(ILogger<TokenController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[Route("/token")]
+        [HttpPost]
+        public IActionResult CreateToken(string userIdentifier)
+        {
+            return new ObjectResult(GenerateToken(userIdentifier));
+        }
+
+        private string GenerateToken(string userIdentifier)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim (ClaimTypes.Name, userIdentifier),
+                new Claim (JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim (JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddMinutes(10)).ToUnixTimeSeconds().ToString()),
+                new Claim (ClaimTypes.Role, "zzz-12" ),
+            };
+
+            var token = new JwtSecurityToken(
+                new JwtHeader(
+                    new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecurityKeyRandomStoreInKeyVault")),
+                        SecurityAlgorithms.HmacSha256)),
+                new JwtPayload(claims));
+
+
+            var jwtSeriToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwtSeriToken;
+        }
+    }
+}
